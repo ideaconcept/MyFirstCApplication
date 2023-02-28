24d82b43d52d740e26247a6c2f853f3ce240ac19
Jacek Hawrylczak <jacek.hawrylczak@ideaconcept.expert>
2023-02-28 20:58:00 +0100

    Zadanie dzień 4

--- MyFirstCApplication/Program.cs
+++ MyFirstCApplication/Program.cs
@@ -1,13 +1,12 @@
 ﻿// Deklaracja zmiennych
-string name = "Adam";
-int myAge = 30;
+string name = "Ewa";
+int age = 17;
 char sex = 'M';
 
-// Algorytm
 // Wyświetl dane wejściowe
 Console.WriteLine("Dane wejściowe:");
 Console.WriteLine("Imie osoby: " + name);
-Console.WriteLine("Wiek osoby: " + myAge + " lat");
+Console.WriteLine("Wiek osoby: " + age + " lat");
 if (sex == 'K')
 {
     Console.WriteLine("Płeć: Kobieta");
@@ -20,13 +19,29 @@ else
 // Wyświetl wynik analizy danych
 Console.WriteLine(" ");
 Console.WriteLine("Wynik analizy danych:");
-
-
-Console.WriteLine("Kobieta poniżej 30 lat");
-
-Console.WriteLine("Ewa, lat 33");
-
-Console.WriteLine("Niepełnoletni Mężczyzna");
-
-Console.WriteLine("Wprowadzone dane nie spełniają zadanych kryteriów analizy.");
-
+if (sex == 'K')
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age == 33 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else
+    {
+        Console.WriteLine("Wprowadzone dane nie spełniają zadanych kryteriów analizy. Zmień dane wejściowe.");
+    }
+ }
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Wprowadzone dane nie spełniają zadanych kryteriów analizy. Zmień dane wejściowe.");
+    }
+}
