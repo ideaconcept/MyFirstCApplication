1562419bfe340245e1d99dcca0d2e36e8226d326
ideaconcept <jacek.hawrylczak@ideaconcept.expert>
2023-02-28 17:19:05 +0100

    Zadanie dzień 4

--- MyFirstCApplication/Program.cs
+++ MyFirstCApplication/Program.cs
@@ -1,2 +1,32 @@
-﻿Console.WriteLine("Cześć Jacku!");
-Console.WriteLine("Mamy dziś piękny dzień!!! Weź się do roboty!!!!! :-)");
+﻿// Deklaracja zmiennych
+string name = "Adam";
+int myAge = 30;
+char sex = 'M';
+
+// Algorytm
+// Wyświetl dane wejściowe
+Console.WriteLine("Dane wejściowe:");
+Console.WriteLine("Imie osoby: " + name);
+Console.WriteLine("Wiek osoby: " + myAge + " lat");
+if (sex == 'K')
+{
+    Console.WriteLine("Płeć: Kobieta");
+}
+else
+{
+    Console.WriteLine("Płeć: Mężczyzna");
+ }
+
+// Wyświetl wynik analizy danych
+Console.WriteLine(" ");
+Console.WriteLine("Wynik analizy danych:");
+
+
+Console.WriteLine("Kobieta poniżej 30 lat");
+
+Console.WriteLine("Ewa, lat 33");
+
+Console.WriteLine("Niepełnoletni Mężczyzna");
+
+Console.WriteLine("Wprowadzone dane nie spełniają zadanych kryteriów analizy.");
+
