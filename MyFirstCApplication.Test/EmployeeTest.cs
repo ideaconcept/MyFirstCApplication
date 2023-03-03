ca0ae5d0d49603187bb88fc2a9bbbd382cc15f03
Jacek Hawrylczak <jacek.hawrylczak@ideaconcept.expert>
2023-03-03 20:27:40 +0100

    Zadanie Dzień 7

--- MyFirstCApplication.Test/EmployeeTest.cs
+++ MyFirstCApplication.Test/EmployeeTest.cs
@@ -3,7 +3,7 @@ namespace MyFirstCApplication.Test
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void AddingRatings()
         {
             var employee = new Employee("Adam", "Nowak", 38);
             employee.AddScore(8);
@@ -14,5 +14,30 @@ namespace MyFirstCApplication.Test
             Assert.AreEqual(15, result);
 
         }
+        [Test]
+        public void AddingRatingsWithANegativeRating()
+        {
+            var employee = new Employee("Adam", "Nowak", 38);
+            employee.AddScore(8);
+            employee.AddScore(-2);
+            employee.AddScore(-10);
+
+            var result = employee.Result;
+
+            Assert.AreEqual(-4, result);
+
+        }
+        [Test]
+        public void AddNewEmployee()
+        {
+            var employee = new Employee("Adam", "Nowak", 38);
+
+
+            var result = employee.Name + employee.Surname + employee.Age;
+
+            Assert.AreEqual("AdamNowak38", result);
+
+        }
+
     }
 }
